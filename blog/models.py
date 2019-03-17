d018b4c99a9881d466a68d1cf56f1267629b3ebc
Larry Ayden Lim <larryayden@Larrys-MacBook-Pro.local>
2019-03-17 17:08:04 +0800

    jobs and blogs all done

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     post_date = models.DateTimeField()
     post_body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.post_body[:50]
+
+    def post_date_pretty(self):
+        return self.post_date.strftime('%b %e %Y')
